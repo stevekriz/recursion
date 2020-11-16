d301aa14e896251a2800f70c9e320c56fa018701
Steve Kriz <steve.kriz@gmail.com>
2020-11-16 12:31:21 -0800

    Implemented stringifyJSON through arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    let result = [];
+    for (let i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  }
+  if (obj instanceof Object) {
+    let result = [];
+    var objectKeys = Object.keys(obj);
+    objectKeys.forEach(function(key) {
+      var keyOut = '"' + key + '"';
+      var keyValue = obj[key];
+      if (typeof keyValue === 'string') {
+        result.push(keyOut + '"' + keyValue + '"');
+      } else if (keyValue instanceof Object) {
+        result.push(keyOut + stringifyJson(keyValue));
+      } else {
+        result.push(keyOut + keyValue);
+      }
+      return '{' + result + '}';
+    });
+  }
+  return '' + obj;
 };
